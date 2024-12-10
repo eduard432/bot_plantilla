39394464ba37a3aa880816878c9f7d24e62ed1fe
eduard432 <juantokx@gmail.com>
2024-12-10 17:07:27 -0600

    Added WA Client

--- clients/wa_terminal/index.ts
+++ clients/wa_terminal/index.ts
@@ -1,1 +1,25 @@
-console.log('first')
+import { Client, LocalAuth } from 'whatsapp-web.js'
+import qrcode from 'qrcode-terminal'
+
+
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox']
+    },
+    authStrategy: new LocalAuth({
+        dataPath: './auth_session'
+    })
+})
+
+client.once('ready', () => {
+    console.log('Client is ready!!')
+})
+
+client.once('disconnected', () => {
+    console.log('Client offline!!')
+})
+
+client.on('qr', (qrString) => {
+    console.log('QR READY!!')
+    qrcode.generate(qrString)
+})
